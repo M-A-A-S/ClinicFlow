b24f43960b5fd05fcc18bf5b011da25bd39cb73f
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-01 13:42:43 +0200

    Add ChronicConditions Controller

---
+++ ClinicFlow.WebUI/ViewModels/ChronicCondition/ChronicConditionIndexVM.cs
@@ -0,0 +1,13 @@
+﻿using ClinicFlow.Domain.DTOs.Allergy;
+using ClinicFlow.Domain.DTOs.ChronicCondition;
+using ClinicFlow.Domain.Utilities;
+
+namespace ClinicFlow.WebUI.ViewModels.ChronicCondition
+{
+    public class ChronicConditionIndexVM
+    {
+        public PagedResult<ChronicConditionDTO> PagedResult { get; set; } = new();
+        public ChronicConditionFilterDTO Filter { get; set; } = new();
+
+    }
+}
