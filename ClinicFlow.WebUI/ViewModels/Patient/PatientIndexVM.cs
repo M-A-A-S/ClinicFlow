d1ecfe5fb1b5c6499c45561b4a30827be019c0b9
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-01 15:20:37 +0200

    add Patients Controller

---
+++ ClinicFlow.WebUI/ViewModels/Patient/PatientIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using ClinicFlow.Domain.DTOs.ChronicCondition;
+using ClinicFlow.Domain.DTOs.Patient;
+using ClinicFlow.Domain.Utilities;
+
+namespace ClinicFlow.WebUI.ViewModels.Patient
+{
+    public class PatientIndexVM
+    {
+        public PagedResult<PatientDTO> PagedResult { get; set; } = new();
+        public PatientFilterDTO Filter { get; set; } = new();
+    }
+}
