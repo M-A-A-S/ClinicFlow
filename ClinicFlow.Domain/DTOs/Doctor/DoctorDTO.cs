1808a234ca6f4d0de573d89e7273a382e9d24028
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-05 12:10:29 +0200

    Add Doctors DTOS

---
+++ ClinicFlow.Domain/DTOs/Doctor/DoctorDTO.cs
@@ -0,0 +1,54 @@
+﻿using ClinicFlow.Domain.DTOs.DoctorSpecialty;
+using ClinicFlow.Domain.Resources.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicFlow.Domain.DTOs.Doctor
+{
+    public class DoctorDTO
+    {
+        public int Id { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.FullName),
+            ResourceType = typeof(SharedResource)
+        )]
+        [Required(
+            ErrorMessageResourceName = nameof(SharedResource.Required),
+            ErrorMessageResourceType = typeof(SharedResource)
+        )]
+        public string FullName { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.PhoneNumber),
+            ResourceType = typeof(SharedResource)
+        )]
+        public string? PhoneNumber { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.Email),
+            ResourceType = typeof(SharedResource)
+        )]
+        public string? Email { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.ConsultationFee),
+            ResourceType = typeof(SharedResource)
+        )]
+        public decimal? ConsultationFee { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.IsActive),
+            ResourceType = typeof(SharedResource)
+        )]
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<DoctorSpecialtyDTO> DoctorSpecialties { get; set; }
+            = new List<DoctorSpecialtyDTO>();
+
+    }
+}
---
+++ ClinicFlow.Domain/DTOs/DoctorSpecialty/DoctorSpecialtyDTO.cs
@@ -0,0 +1,39 @@
+﻿using ClinicFlow.Domain.DTOs.Doctor;
+using ClinicFlow.Domain.DTOs.Specialty;
+using ClinicFlow.Domain.Resources.Shared;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicFlow.Domain.DTOs.DoctorSpecialty
+{
+    public class DoctorSpecialtyDTO
+    {
+        public int Id { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.Doctor),
+            ResourceType = typeof(SharedResource)
+        )]
+        [Required(
+            ErrorMessageResourceName = nameof(SharedResource.Required),
+            ErrorMessageResourceType = typeof(SharedResource)
+        )]
+        public int DoctorId { get; set; }
+        public DoctorDTO? Doctor { get; set; }
+
+        [Display(
+            Name = nameof(SharedResource.Specialty),
+            ResourceType = typeof(SharedResource)
+        )]
+        [Required(
+            ErrorMessageResourceName = nameof(SharedResource.Required),
+            ErrorMessageResourceType = typeof(SharedResource)
+        )]
+        public int SpecialtyId { get; set; }
+        public SpecialtyDTO? Specialty { get; set; }
+    }
+}
