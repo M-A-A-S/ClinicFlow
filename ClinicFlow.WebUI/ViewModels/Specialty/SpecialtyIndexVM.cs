9f09bd20df9db923b2210e508ea4f4fe46c5d328
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-05 12:21:03 +0200

    Add Specialties Controller

---
+++ ClinicFlow.WebUI/ViewModels/Specialty/SpecialtyIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using ClinicFlow.Domain.DTOs.Specialty;
+using ClinicFlow.Domain.Utilities;
+
+namespace ClinicFlow.WebUI.ViewModels.Specialty
+{
+    public class SpecialtyIndexVM
+    {
+        public PagedResult<SpecialtyDTO> PagedResult { get; set; } = new();
+        public SpecialtyFilterDTO Filter { get; set; } = new();
+
+    }
+}
