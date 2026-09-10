70c29a2e43183a07d3fe3c2ff5835dbe9538d621
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-10 13:56:31 +0200

    Add Visit Index view

--- ClinicFlow.Infrastructure/DependencyInjection.cs
+++ ClinicFlow.Infrastructure/DependencyInjection.cs
@@ -20,6 +20,7 @@ namespace ClinicFlow.Infrastructure
             IConfiguration configuration)
         {
 
+            services.AddScoped<IVisitService, VisitService>();
             services.AddScoped<IMedicineService, MedicineService>();
             services.AddScoped<IWaitingQueueService, WaitingQueueService>();
             services.AddScoped<IClinicDoctorService, ClinicDoctorService>();
