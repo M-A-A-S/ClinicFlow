c7ed99811f72a18a0a2d72184278211d285086f4
Mohammed Alfatih <mohammedalfatih606@gmail.com>
2026-09-17 14:35:35 +0200

    Add LabResults Controller

---
+++ ClinicFlow.WebUI/Controllers/LabResultsController.cs
@@ -0,0 +1,181 @@
+﻿using ClinicFlow.Application.Services;
+using ClinicFlow.Domain.DTOs.LabResult;
+using ClinicFlow.Domain.Resources.Shared;
+using ClinicFlow.Domain.Utilities;
+using ClinicFlow.WebUI.ViewModels.LabResult;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+
+namespace ClinicFlow.WebUI.Controllers
+{
+    public class LabResultsController : BaseController
+    {
+
+        #region ========================= Fields & Properties =========================
+        private readonly ILabResultService _service;
+
+        #endregion
+
+        #region ========================= Constructors =========================
+        public LabResultsController(
+            ILabResultService service,
+            IStringLocalizer<SharedResource> localizer
+
+            ) : base(localizer)
+        {
+            _service = service;
+        }
+        #endregion
+
+        #region ========================= Get =========================
+        public async Task<IActionResult> Index(LabResultFilterDTO filter)
+        {
+            var getAllResult = await _service.GetAllAsync(filter);
+
+            if (!getAllResult.IsSuccess)
+            {
+                Error(getAllResult.Code);
+            }
+
+            var viewModel = new LabResultIndexVM
+            {
+                PagedResult = getAllResult.Data ?? new PagedResult<LabResultDTO>(),
+                Filter = filter,
+            };
+
+
+            return View(viewModel);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+
+            var item = await GetEntityOrNull(_service.GetByIdAsync(id));
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        #endregion
+
+        #region ========================= Create =========================
+        public async Task<IActionResult> Create(int labOrderItemId)
+        {
+            var item = await GetEntityOrNull(_service.GetByOrderItemIdAsync(labOrderItemId));
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(LabResultDTO DTO)
+        {
+            if (InvalidModel())
+            {
+
+                return View(DTO);
+            }
+
+            var addResult = await _service.AddAsync(DTO);
+
+            if (!addResult.IsSuccess)
+            {
+                Error(addResult.Code);
+                return View(DTO);
+            }
+
+            Success(addResult.Code);
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region ========================= Update =========================
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = await GetEntityOrNull(_service.GetByIdAsync(id));
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+
+            return View(item);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(LabResultDTO DTO)
+        {
+            if (InvalidModel())
+            {
+                return View(DTO);
+            }
+
+            var updateResult = await _service.UpdateAsync(DTO.Id, DTO);
+            if (!updateResult.IsSuccess)
+            {
+                Error(updateResult.Code);
+                return View(DTO);
+            }
+
+            Success(updateResult.Code);
+            return RedirectToAction(nameof(Index));
+        }
+
+        #endregion
+
+        #region ========================= Delete =========================
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = await GetEntityOrNull(_service.GetByIdAsync(id));
+
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return View(item);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+
+            var deleteResult = await _service.DeleteAsync(id);
+
+            if (!deleteResult.IsSuccess)
+            {
+                Error(deleteResult.Code);
+                var item = await GetEntityOrNull(_service.GetByIdAsync(id));
+                if (item is null)
+                {
+                    return NotFound();
+                }
+                return View(item);
+            }
+
+            Success(deleteResult.Code);
+            return RedirectToAction(nameof(Index));
+        }
+        #endregion
+
+        #region ========================= Helpers =========================
+
+
+        #endregion
+
+    }
+}
---
+++ ClinicFlow.WebUI/ViewModels/LabResult/LabResultIndexVM.cs
@@ -0,0 +1,12 @@
+﻿using ClinicFlow.Domain.DTOs.LabResult;
+using ClinicFlow.Domain.Utilities;
+
+namespace ClinicFlow.WebUI.ViewModels.LabResult
+{
+    public class LabResultIndexVM
+    {
+        public PagedResult<LabResultDTO> PagedResult { get; set; } = new();
+        public LabResultFilterDTO Filter { get; set; } = new();
+
+    }
+}
